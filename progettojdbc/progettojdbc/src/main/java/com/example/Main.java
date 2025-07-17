ca588c1a4932831f1ba6102fbeddd72b17062911
Deborah930 <rolesudeborah@gmail.com>
2025-07-17 11:57:48 +0200

    Update Main.java

--- progettojdbc/progettojdbc/src/main/java/com/example/Main.java
+++ progettojdbc/progettojdbc/src/main/java/com/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/sakila"; // sostituire con il nome del DB
         String user = "root"; // utente del DB
-        String password = "Farfalla930!"; // password del DB
+        String password = "mia password!"; // password del DB
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
 

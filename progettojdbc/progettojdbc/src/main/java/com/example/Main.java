f63b8a4c77bc68e5a58bd8596fe8729eff22b5f5
Deborah930 <rolesudeborah@gmail.com>
2025-07-17 11:54:51 +0200

    Esercizio1jdbc

--- progettojdbc/progettojdbc/src/main/java/com/example/Main.java
+++ progettojdbc/progettojdbc/src/main/java/com/example/Main.java
@@ -1,20 +1,39 @@
 package com.example;
 
-
-
 import java.sql.*;
-        
-        public class Main {
-            public static void main(String[] args) {
-                String url = "jdbc:mysql://localhost:3306/world"; // sostituire con il nome del DB
-                String user = "root";       // utente del DB
-                String password = "MIA PASSWORD";       // password del DB
-        
-                try (Connection conn = DriverManager.getConnection(url, user, password)) {
-                    System.out.println("Connessione riuscita");
-                } catch (SQLException e) {
-                    System.out.println("Errore nella connessione");
-                    e.printStackTrace();
-                }
-            }
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/sakila"; // sostituire con il nome del DB
+        String user = "root"; // utente del DB
+        String password = "Farfalla930!"; // password del DB
+
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+
+            System.out.println("Connessione riuscita");
+        } catch (SQLException e) {
+            System.out.println("Errore nella connessione");
+            e.printStackTrace();
         }
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+
+             Statement stmt = conn.createStatement();
+
+             ResultSet rs = stmt.executeQuery("SELECT title FROM film LIMIT 5")) {
+
+
+            while (rs.next()) {
+
+                System.out.println("Film: " + rs.getString("title"));
+
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+}
